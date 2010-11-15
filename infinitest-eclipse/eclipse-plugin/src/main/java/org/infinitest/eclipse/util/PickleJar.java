b996d4e7f72a97f8c60ddbf7a8186bd90ed5ea3a
Eric Lefevre-Ardant <eric.lefevre@algodeal.com>
2010-11-15 12:46:46 +0100

    Fixed serialization on Linux 64 bits (cherry picked from commit 8d290d8af447965ad42af61620f6b3c3cea2bdbc)
    Signed-off-by: David Gageot <david@gageot.net>

--- infinitest-eclipse/eclipse-plugin/src/main/java/org/infinitest/eclipse/util/PickleJar.java
+++ infinitest-eclipse/eclipse-plugin/src/main/java/org/infinitest/eclipse/util/PickleJar.java
@@ -25,15 +25,17 @@ import static org.apache.commons.lang.SerializationUtils.*;
 
 import java.io.Serializable;
 
+import org.apache.commons.codec.binary.Base64;
+
 public abstract class PickleJar
 {
     public static String pickle(Serializable object)
     {
-        return new String(serialize(object));
+        return new String(Base64.encodeBase64(serialize(object)));
     }
 
     public static Serializable unpickle(String stringForm)
     {
-        return (Serializable) deserialize(stringForm.getBytes());
+        return (Serializable) deserialize(Base64.decodeBase64(stringForm.getBytes()));
     }
 }
