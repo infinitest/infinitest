b6219c48a591c0fc194955f1671bcc97684c2030
David Gageot <david@gageot.net>
2010-11-18 10:37:10 +0100

    Better publish process without a site submodule

--- publish.sh
+++ publish.sh
@@ -1,63 +1,65 @@
 #!/bin/sh
 abspath="$(cd "${0%/*}" 2>/dev/null; echo "$PWD"/"${0##*/}")"
-eclipse_root=`dirname $abspath`
-project_root=`dirname $eclipse_root`
-main_site=$project_root/website/site
+project_root=`dirname ${abspath}`
+main_site=${project_root}/../infinitest.github.com
 update_site=${main_site}/experimental
 
-# Increment Version
-# DEBT Replace some of this hackery with a maven properties file
-current_minor_version=`perl -nle 'while(m/(<plugin.version>)(\d\.\d\.)(\d*)(<\/plugin.version>)/g){print $2}' pom.xml`
-current_point_version=`perl -nle 'while(m/(<plugin.version>)(\d\.\d\.)(\d*)(<\/plugin.version>)/g){print $3}' pom.xml`
-new_version=$current_minor_version`expr $current_point_version + 1`
-sed s/\<plugin.version\>$current_minor_version$current_point_version/\<plugin.version\>$new_version/g pom.xml > pom.xml.new
-mv pom.xml.new pom.xml
-
-# Move up to root of repo
-cd $project_root 
-
-# Build it!
-mvn clean install -Pintegration
-if [ "$?" -ne "0" ]; then
-    cd $project_root
-    git checkout infinitest-eclipse/pom.xml
-    echo "Build Failure. Exiting..."
-    exit 1
+# Test for web project existence
+if [ ! -e ${main_site} ]; then
+	echo "You should clone infinitest.github.com project at the same level as infinitest project."
+	exit 1
 fi
 
+# Clean working directory
+cd ${project_root}
+git stash save --quiet "Before publish"
+git clean -df
+
+# Ask for version summary
 echo "Please enter the newsfeed summary for this release:"
 read release_message
 
-# Prompt and update if accepted
-echo "Ready to upload version $new_version to ${update_site} site with message: $release_message"
-echo "Publish now?"
-select fname in [p]ush,[a]bort;
-do
-  if [ $REPLY = "p" ]; then
-    mkdir -p ${update_site}/features
-    mkdir -p ${update_site}/plugins
-    echo "Pushing to update site..."
-    cd $project_root/infinitest-eclipse
-    printf "\n\n%s    %s    %s" "`date`" ${new_version} "${release_message}" >> ReleaseNotes.txt
-    ruby update_rss.rb ${new_version} "${release_message}"
-    cp rss.xml ${update_site}/
-    cp ReleaseNotes.txt ${update_site}/
-    cp site.xml ${update_site}/
-    cp target/org.infinitest.eclipse.feature_${new_version}.jar ${update_site}/features/
-    cp target/org.infinitest.eclipse_${new_version}.jar ${update_site}/plugins/
+# Increment Version
+CURRENT=`grep "<version>" -i pom.xml --max-count 1 | sed -e "s/.*<version>\(.*\)<\/version>/\1/" | sed -e "s/-SNAPSHOT//"`
+MAJOR_VERSION=`echo $CURRENT | sed -e "s/\([0-9]*\.[0-9]*\.\)\([0-9]*\)/\1/"`
+MINOR_VERSION=`echo $CURRENT | sed -e "s/\([0-9]*\.[0-9]*\.\)\([0-9]*\)/\2/"`
+NEXT=$MAJOR_VERSION`expr $MINOR_VERSION + 1`-SNAPSHOT
+TAG=VERSION${CURRENT}
 
-    #git commit -a -m "Releasing $new_version -- $release_message"
-    #git push origin master
+find . -name "*.xml" | xargs perl -pi -e "s/${CURRENT}-SNAPSHOT/${NEXT}/g"
+git stash save "Preparing ${NEXT}"
+find . -name "*.xml" | xargs perl -pi -e "s/${CURRENT}-SNAPSHOT/${CURRENT}/g"
 
-    #cd $update_site
-    #git commit -am  "Released version $new_version"
-    #git push origin master
-    break
-  else
-    echo "Aborted!"
-    cd $project_root/infinitest-eclipse
-    git checkout pom.xml
-    break
-  fi
-  break;
-done
+# Commit main project
+git commit -am "${release_message}"
+git tag -f "$TAG" -m "$TAG"
+
+# Build it!
+mvn clean install -Pintegration
+if [ "$?" -ne 0 ]; then
+	echo "ERROR> Maven build failed aborting."
+	git reset --hard HEAD~1
+	git tag -d "$TAG"
+	exit 1
+fi
+
+# Prepare next version
+git stash pop --quiet
+git checkout --theirs "*.xml"
+git commit -am "${NEXT}"
+
+# Publish web site
+cd ${main_site}
+git clean -df
+cp -r ${project_root}/infinitest-eclipse/target/update_site/* ${update_site}
+printf "\n\n%s    %s    %s" "`date`" ${CURRENT} "${release_message}" >> ${update_site}/ReleaseNotes.txt
+
+cd  ${update_site}
+ruby update_rss.rb ${CURRENT} "${release_message}"
+rm update_rss.rb
+
+cd ${main_site}
+git add .
+git commit -am "${release_message}"
+git tag -f "$TAG" -m "$TAG"
+
