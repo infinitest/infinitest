b15f41fe8e4e69154c3f705c6fc0a237843c6a71
David Gageot <david@gageot.net>
2013-04-18 10:05:02 +0200

    Remove mock helper

--- infinitest-eclipse/src/test/java/org/infinitest/eclipse/workspace/WhenResolvingClassPath.java
+++ infinitest-eclipse/src/test/java/org/infinitest/eclipse/workspace/WhenResolvingClassPath.java
@@ -33,106 +33,111 @@ import static org.mockito.Mockito.*;
 import org.eclipse.core.runtime.*;
 import org.eclipse.jdt.core.*;
 import org.eclipse.jdt.launching.*;
-import org.infinitest.util.*;
 import org.junit.*;
 import org.junit.runner.*;
+import org.mockito.*;
+import org.mockito.runners.*;
 
-@RunWith(AutoMockRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 public class WhenResolvingClassPath {
-	private EclipseFacade mockEclipseFacade;
+	@InjectMocks
 	private ClassPathResolver classPathResolver;
-	private IJavaProject mockProject;
-	private IRuntimeClasspathEntry mockRuntimeClasspathEntry;
-	private IPath mockPath;
-	private IPath mockJarPath;
-	private IClasspathContainer mockIClasspathContainer;
-	private IClasspathEntry mockIClasspathEntry;
 
-	@Before
-	public void inContext() {
-		classPathResolver = new ClassPathResolver(mockEclipseFacade);
-	}
+	@Mock
+	private EclipseFacade eclipseFacade;
+	@Mock
+	private IJavaProject project;
+	@Mock
+	private IRuntimeClasspathEntry runtimeClasspathEntry;
+	@Mock
+	private IPath path;
+	@Mock
+	private IPath jarPath;
+	@Mock
+	private IClasspathContainer classpathContainer;
+	@Mock
+	private IClasspathEntry classpathEntry;
 
 	@Test
 	public void shouldFindDefaultRuntimeClassPathWithOneJar() throws CoreException {
-		when(mockEclipseFacade.computeDefaultRuntimeClassPath(mockProject)).thenReturn(new String[] { "1.jar" });
-		when(mockEclipseFacade.computeUnresolvedRuntimeClasspath(mockProject)).thenReturn(new IRuntimeClasspathEntry[0]);
+		when(eclipseFacade.computeDefaultRuntimeClassPath(project)).thenReturn(new String[]{"1.jar"});
+		when(eclipseFacade.computeUnresolvedRuntimeClasspath(project)).thenReturn(new IRuntimeClasspathEntry[0]);
 
-		String classpath = classPathResolver.rawClasspath(mockProject);
+		String classpath = classPathResolver.rawClasspath(project);
 
 		assertThat(classpath).isEqualTo("1.jar");
 	}
 
 	@Test
 	public void shouldFindDefaultRuntimeClassPathWithTwoJars() throws CoreException {
-		when(mockEclipseFacade.computeDefaultRuntimeClassPath(mockProject)).thenReturn(new String[] { "1.jar", "2.jar" });
-		when(mockEclipseFacade.computeUnresolvedRuntimeClasspath(mockProject)).thenReturn(new IRuntimeClasspathEntry[0]);
+		when(eclipseFacade.computeDefaultRuntimeClassPath(project)).thenReturn(new String[]{"1.jar", "2.jar"});
+		when(eclipseFacade.computeUnresolvedRuntimeClasspath(project)).thenReturn(new IRuntimeClasspathEntry[0]);
 
-		String classpath = classPathResolver.rawClasspath(mockProject);
+		String classpath = classPathResolver.rawClasspath(project);
 
 		assertThat(classpath).isEqualTo("1.jar:2.jar");
 	}
 
 	@Test
 	public void shouldAddUnresolvedRuntimeJars() throws CoreException {
-		when(mockEclipseFacade.computeDefaultRuntimeClassPath(mockProject)).thenReturn(new String[] { "1.jar", "2.jar" });
-		when(mockEclipseFacade.computeUnresolvedRuntimeClasspath(mockProject)).thenReturn(new IRuntimeClasspathEntry[] { mockRuntimeClasspathEntry });
-		when(mockRuntimeClasspathEntry.getVariableName()).thenReturn("SCALA_CONTAINER");
-		when(mockRuntimeClasspathEntry.getPath()).thenReturn(mockPath);
-		when(mockEclipseFacade.getClasspathContainer(mockPath, mockProject)).thenReturn(mockIClasspathContainer);
-		when(mockIClasspathContainer.getClasspathEntries()).thenReturn(new IClasspathEntry[] { mockIClasspathEntry });
-		when(mockIClasspathEntry.getPath()).thenReturn(mockJarPath);
-		when(mockJarPath.toString()).thenReturn("3.jar");
+		when(eclipseFacade.computeDefaultRuntimeClassPath(project)).thenReturn(new String[]{"1.jar", "2.jar"});
+		when(eclipseFacade.computeUnresolvedRuntimeClasspath(project)).thenReturn(new IRuntimeClasspathEntry[]{runtimeClasspathEntry});
+		when(runtimeClasspathEntry.getVariableName()).thenReturn("SCALA_CONTAINER");
+		when(runtimeClasspathEntry.getPath()).thenReturn(path);
+		when(eclipseFacade.getClasspathContainer(path, project)).thenReturn(classpathContainer);
+		when(classpathContainer.getClasspathEntries()).thenReturn(new IClasspathEntry[]{classpathEntry});
+		when(classpathEntry.getPath()).thenReturn(jarPath);
+		when(jarPath.toString()).thenReturn("3.jar");
 
-		String classpath = classPathResolver.rawClasspath(mockProject);
+		String classpath = classPathResolver.rawClasspath(project);
 
 		assertThat(classpath).isEqualTo("1.jar:2.jar:3.jar");
 	}
 
 	@Test
 	public void shouldIgnoreJreContainer() throws CoreException {
-		when(mockEclipseFacade.computeDefaultRuntimeClassPath(mockProject)).thenReturn(new String[0]);
-		when(mockEclipseFacade.computeUnresolvedRuntimeClasspath(mockProject)).thenReturn(new IRuntimeClasspathEntry[] { mockRuntimeClasspathEntry });
-		when(mockRuntimeClasspathEntry.getVariableName()).thenReturn("org.eclipse.jdt.launching.JRE_CONTAINER");
-		when(mockRuntimeClasspathEntry.getPath()).thenReturn(mockPath);
-		when(mockEclipseFacade.getClasspathContainer(mockPath, mockProject)).thenReturn(mockIClasspathContainer);
-		when(mockIClasspathContainer.getClasspathEntries()).thenReturn(new IClasspathEntry[] { mockIClasspathEntry });
-		when(mockIClasspathEntry.getPath()).thenReturn(mockJarPath);
-		when(mockJarPath.toString()).thenReturn("3.jar");
+		when(eclipseFacade.computeDefaultRuntimeClassPath(project)).thenReturn(new String[0]);
+		when(eclipseFacade.computeUnresolvedRuntimeClasspath(project)).thenReturn(new IRuntimeClasspathEntry[]{runtimeClasspathEntry});
+		when(runtimeClasspathEntry.getVariableName()).thenReturn("org.eclipse.jdt.launching.JRE_CONTAINER");
+		when(runtimeClasspathEntry.getPath()).thenReturn(path);
+		when(eclipseFacade.getClasspathContainer(path, project)).thenReturn(classpathContainer);
+		when(classpathContainer.getClasspathEntries()).thenReturn(new IClasspathEntry[]{classpathEntry});
+		when(classpathEntry.getPath()).thenReturn(jarPath);
+		when(jarPath.toString()).thenReturn("3.jar");
 
-		String classpath = classPathResolver.rawClasspath(mockProject);
+		String classpath = classPathResolver.rawClasspath(project);
 
 		assertThat(classpath).isEmpty();
 	}
 
 	@Test
 	public void shouldIgnoreNonJar() throws CoreException {
-		when(mockEclipseFacade.computeDefaultRuntimeClassPath(mockProject)).thenReturn(new String[0]);
-		when(mockEclipseFacade.computeUnresolvedRuntimeClasspath(mockProject)).thenReturn(new IRuntimeClasspathEntry[] { mockRuntimeClasspathEntry });
-		when(mockRuntimeClasspathEntry.getVariableName()).thenReturn("SCALA_CONTAINER");
-		when(mockRuntimeClasspathEntry.getPath()).thenReturn(mockPath);
-		when(mockEclipseFacade.getClasspathContainer(mockPath, mockProject)).thenReturn(mockIClasspathContainer);
-		when(mockIClasspathContainer.getClasspathEntries()).thenReturn(new IClasspathEntry[] { mockIClasspathEntry });
-		when(mockIClasspathEntry.getPath()).thenReturn(mockJarPath);
-		when(mockJarPath.toString()).thenReturn("3.zip");
+		when(eclipseFacade.computeDefaultRuntimeClassPath(project)).thenReturn(new String[0]);
+		when(eclipseFacade.computeUnresolvedRuntimeClasspath(project)).thenReturn(new IRuntimeClasspathEntry[]{runtimeClasspathEntry});
+		when(runtimeClasspathEntry.getVariableName()).thenReturn("SCALA_CONTAINER");
+		when(runtimeClasspathEntry.getPath()).thenReturn(path);
+		when(eclipseFacade.getClasspathContainer(path, project)).thenReturn(classpathContainer);
+		when(classpathContainer.getClasspathEntries()).thenReturn(new IClasspathEntry[]{classpathEntry});
+		when(classpathEntry.getPath()).thenReturn(jarPath);
+		when(jarPath.toString()).thenReturn("3.zip");
 
-		String classpath = classPathResolver.rawClasspath(mockProject);
+		String classpath = classPathResolver.rawClasspath(project);
 
 		assertThat(classpath).isEmpty();
 	}
 
 	@Test
 	public void shouldIgnoreCase() throws CoreException {
-		when(mockEclipseFacade.computeDefaultRuntimeClassPath(mockProject)).thenReturn(new String[0]);
-		when(mockEclipseFacade.computeUnresolvedRuntimeClasspath(mockProject)).thenReturn(new IRuntimeClasspathEntry[] { mockRuntimeClasspathEntry });
-		when(mockRuntimeClasspathEntry.getVariableName()).thenReturn("SCALA_CONTAINER");
-		when(mockRuntimeClasspathEntry.getPath()).thenReturn(mockPath);
-		when(mockEclipseFacade.getClasspathContainer(mockPath, mockProject)).thenReturn(mockIClasspathContainer);
-		when(mockIClasspathContainer.getClasspathEntries()).thenReturn(new IClasspathEntry[] { mockIClasspathEntry });
-		when(mockIClasspathEntry.getPath()).thenReturn(mockJarPath);
-		when(mockJarPath.toString()).thenReturn("3.JAR");
+		when(eclipseFacade.computeDefaultRuntimeClassPath(project)).thenReturn(new String[0]);
+		when(eclipseFacade.computeUnresolvedRuntimeClasspath(project)).thenReturn(new IRuntimeClasspathEntry[]{runtimeClasspathEntry});
+		when(runtimeClasspathEntry.getVariableName()).thenReturn("SCALA_CONTAINER");
+		when(runtimeClasspathEntry.getPath()).thenReturn(path);
+		when(eclipseFacade.getClasspathContainer(path, project)).thenReturn(classpathContainer);
+		when(classpathContainer.getClasspathEntries()).thenReturn(new IClasspathEntry[]{classpathEntry});
+		when(classpathEntry.getPath()).thenReturn(jarPath);
+		when(jarPath.toString()).thenReturn("3.JAR");
 
-		String classpath = classPathResolver.rawClasspath(mockProject);
+		String classpath = classPathResolver.rawClasspath(project);
 
 		assertThat(classpath).isEqualTo("3.JAR");
 	}
