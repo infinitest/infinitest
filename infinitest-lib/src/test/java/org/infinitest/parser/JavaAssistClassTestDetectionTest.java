c95f7c26e46fe7ada423893a5126006dbf13af77
gtoison <guillaume.toison@tobam.fr>
2023-06-19 12:27:30 +0100

    fix: ignored tests are still considered as tests

--- infinitest-lib/src/test/java/org/infinitest/parser/JavaAssistClassTestDetectionTest.java
+++ infinitest-lib/src/test/java/org/infinitest/parser/JavaAssistClassTestDetectionTest.java
@@ -73,10 +73,10 @@ class JavaAssistClassTestDetectionTest {
 		
 		// Ignored/disabled tests are still considered as tests, we leave it to the test engine to decide if a test must run
 		// We cannot always evaluate conditions (for instance @DisabledForJreRange)t
-		assertThat(classPoolUtil.getClass(IgnoredClassJUnit4TestCase.class).isATest()).isFalse();
-		assertThat(classPoolUtil.getClass(IgnoredTestJUnit4TestCase.class).isATest()).isFalse();
-		assertThat(classPoolUtil.getClass(DisabledClassJUnit5TestCase.class).isATest()).isFalse();
-		assertThat(classPoolUtil.getClass(DisabledTestJUnit5TestCase.class).isATest()).isFalse();
+		assertThat(classPoolUtil.getClass(IgnoredClassJUnit4TestCase.class).isATest()).isTrue();
+		assertThat(classPoolUtil.getClass(IgnoredTestJUnit4TestCase.class).isATest()).isTrue();
+		assertThat(classPoolUtil.getClass(DisabledClassJUnit5TestCase.class).isATest()).isTrue();
+		assertThat(classPoolUtil.getClass(DisabledTestJUnit5TestCase.class).isATest()).isTrue();
 	}
 
 	@Test
