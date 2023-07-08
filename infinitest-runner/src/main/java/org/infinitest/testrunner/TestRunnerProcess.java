e8c6cd6b8b34e3d4f72fbaa2d6ff2ee1499f606e
gtoison <guillaume.toison@tobam.fr>
2023-07-08 11:38:15 +0200

    fix: removed unused import

--- infinitest-runner/src/main/java/org/infinitest/testrunner/TestRunnerProcess.java
+++ infinitest-runner/src/main/java/org/infinitest/testrunner/TestRunnerProcess.java
@@ -34,7 +34,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectOutputStream;
-import java.lang.reflect.InvocationTargetException;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
